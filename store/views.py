5a6f3584dee145afa0236ee18d0e2a6b5f944e58
ommshi <ommshi@aol.com>
2024-10-07 12:04:43 +0200

    change Dein-Markt

--- store/views.py
+++ store/views.py
@@ -14,190 +14,150 @@ from django.db.models import Q
 import json
 from cart.cart import Cart
 
-
 def search(request):
-    # Determine if they filled the form
+    categories = Category.objects.all()  
     if request.method == "POST":
         searched = request.POST.get('searched', '')  # Safely get the value
-        #Query the products DB models 
         searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
         if not searched:
-             messages.success(request, "Dieses Produkt existiert bei uns nicht.")
-             return render(request, 'search.html', {})
+            messages.success(request, "Dieses Produkt existiert bei uns nicht.")
+            return render(request, 'search.html', {'categories': categories})  
         else:
-             return render(request, 'search.html', {'searched': searched})
-
-    
+            return render(request, 'search.html', {'searched': searched, 'categories': categories})  
     else:
-        return render(request, 'search.html', {})
-    
-
-
-
+        return render(request, 'search.html', {'categories': categories})  
 
 def update_info(request):
+    categories = Category.objects.all()  
     if request.user.is_authenticated:
-        # Aktuellen Benutzer abrufen
         current_user = Profile.objects.get(user__id=request.user.id)
-        # Versandinformationen des aktuellen Benutzers abrufen
         shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
         
-        # Original Benutzerformular abrufen
         form = UserInfoForm(request.POST or None, instance=current_user)
-        # Benutzer Versandformular abrufen
         shipping_form = ShippingForm(request.POST or None, instance=shipping_user)        
         if form.is_valid() or shipping_form.is_valid():
-            # Originalformular speichern
             form.save()
-            # Versandformular speichern
             shipping_form.save()
-
             messages.success(request, "Ihre Informationen wurden aktualisiert!")
             return redirect('update_info')
-        return render(request, "update_info.html", {'form': form, 'shipping_form': shipping_form})
+        return render(request, "update_info.html", {'form': form, 'shipping_form': shipping_form, 'categories': categories})  
     else:
         messages.success(request, "Sie müssen angemeldet sein, um auf diese Seite zugreifen zu können!")
         return redirect('home')
 
-
 def update_password(request):
+    categories = Category.objects.all()  
     if request.user.is_authenticated:
         current_user = request.user
-
         if request.method == 'POST':
             form = ChangePasswordForm(current_user, request.POST)
             if form.is_valid():
                 form.save()
                 messages.success(request, 'Dein Passwort wurde erfolgreich aktualisiert!')
                 login(request, current_user)
-                return redirect('update_user')  # Redirect to the profile page or another relevant page
+                return redirect('update_user')
             else:
                 for error in list(form.errors.values()):
                     messages.error(request, error)
                     return redirect('update_password')
         else:
             form = ChangePasswordForm(current_user)
-            return render(request, 'update_password.html', {'form': form})
-
-
+            return render(request, 'update_password.html', {'form': form, 'categories': categories})  
     else:
         messages.error(request, 'Du musst eingeloggt sein, um dein Passwort zu aktualisieren.')
-        return redirect('home')  # Redirect to login or another page
-
+        return redirect('home')
 
 def update_user(request):
-     if request.user.is_authenticated:
-          current_user = User.objects.get(id=request.user.id)
-          user_form = UpdateUserForm(request.POST or None, instance=current_user)
+    categories = Category.objects.all()  
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user)
 
-          if user_form.is_valid():
-               user_form.save()
-
-               login(request, current_user)
-               messages.success(request, "Profil wurde erfolgreich aktualisiert.")
-               return redirect('home')
-          return render(request, "update_user.html", {'user_form':user_form})
-     else:
-          messages.success(request, "Du musst angemeldet sein, um auf diese Seite zuzugreifen.")
-          return redirect('home')
-
-
-
-
-
+        if user_form.is_valid():
+            user_form.save()
+            login(request, current_user)
+            messages.success(request, "Profil wurde erfolgreich aktualisiert.")
+            return redirect('home')
+        return render(request, "update_user.html", {'user_form': user_form, 'categories': categories})  
+    else:
+        messages.success(request, "Du musst angemeldet sein, um auf diese Seite zuzugreifen.")
+        return redirect('home')
 
 def category_summary(request):
-     categories = Category.objects.all()
-     return render(request, 'category_summary.html', {'categories':categories})
-
+    categories = Category.objects.all()  
+    categories = Category.objects.all()
+    return render(request, 'category_summary.html', {'categories': categories})
 
 def category(request, foo):
-     #replace Hyphen with space
-    foo = foo.replace('-',' ')
-     #grab the category from the url
+    categories = Category.objects.all()  
+    foo = foo.replace('-', ' ')
     try:
-         #look up the category
-         category = Category.objects.get(name=foo)
-         products = Product.objects.filter(category=category)
-         return render(request, 'category.html', {'products':products, 'category':category})
-    
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category': category, 'categories': categories})  
     except:
-         messages.success(request, ("Diese Kategorie existiert nicht."))
-         return redirect('home')
-          
-    
-
+        messages.success(request, ("Diese Kategorie existiert nicht."))
+        return redirect('home')
 
 def product(request, pk):
+    categories = Category.objects.all()  
     product = Product.objects.get(id=pk)
-    return render(request, 'product.html', {'product':product})
-
+    return render(request, 'product.html', {'product': product, 'categories': categories})  
 
 def home(request):
+    categories = Category.objects.all()  
     products = Product.objects.all()
-    return render(request, 'home.html', {'products':products})
-
+    return render(request, 'home.html', {'products': products, 'categories': categories})  
 
 def about(request):
+    categories = Category.objects.all()  
     products = Product.objects.all()
-    return render(request, 'about.html', {})
-
+    return render(request, 'about.html', {'categories': categories})  
 
 def login_user(request):
+    categories = Category.objects.all()  
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            #Do some shopping cart stuff
-            current_user =Profile.objects.get(user__id=request.user.id)
-            #get the saved Cart
+            current_user = Profile.objects.get(user__id=request.user.id)
             saved_cart = current_user.old_cart
-            #convert database string to python dictionary
             if saved_cart:
-                 #convert to dictionary using JSON
-                 converted_cart = json.loads(saved_cart)
-                 #add the loaded cart dictionary to our session
-                 cart = Cart(request)
-                 #loop thru the cart and add items from the database
-                 for key , value in converted_cart.items():
-                      cart.db_add(product=key, quantity=value)
-
+                converted_cart = json.loads(saved_cart)
+                cart = Cart(request)
+                for key, value in converted_cart.items():
+                    cart.db_add(product=key, quantity=value)
 
             messages.success(request, ("Du bist eingeloggt"))
             return redirect('home')
         else:
             return redirect('login')
-
     else:
-        return render(request, 'login.html', {})
-
-
+        return render(request, 'login.html', {'categories': categories})  
 
 def logout_user(request):
+    categories = Category.objects.all()  
     logout(request)
     messages.success(request, ("Bis zum nächsten Mal"))
     return redirect('home')
 
-
-
 def register_user(request):
-            form = SignUpForm()
-            if request.method == "POST":
-                 form = SignUpForm(request.POST)
-                 if form.is_valid():
-                      form.save()
-                      username = form.cleaned_data['username']
-                      password = form.cleaned_data['password1']
-                      #log in user
-                      user = authenticate(username=username, password=password)
-                      login(request, user)
-                      messages.success(request, ("Dein Konto wurde registriert"))
-                      return redirect('home')
-                 else:
-                      messages.success(request, ("Upps, bei der Registrierung ist ein Problem aufgetreten. Bitte versuche es erneut."))
-                      return redirect('register')
-                      
-            else:
-                 return render(request, 'register.html', {'form':form})
+    categories = Category.objects.all()  
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Dein Konto wurde registriert"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Upps, bei der Registrierung ist ein Problem aufgetreten. Bitte versuche es erneut."))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form, 'categories': categories})  
